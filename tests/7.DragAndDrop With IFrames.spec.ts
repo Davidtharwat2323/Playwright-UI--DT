fa0dfd587e2c3b18290c2ea36ba912d2b6dea751
Tharwat <dtharwat@deloitte.com>
2024-11-11 11:49:55 +0200

    Resolve issue in DragAndDrop Script

--- tests/7.DragAndDrop With IFrames.spec.ts
+++ tests/7.DragAndDrop With IFrames.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "@playwright/test"
 //create test cases
 test("open Drag & Drop Site URL", async ({ page }) => {
-    await page.goto("https://www.globalsqa.com/demo-site/draganddrop/");
-
+ 
+   await page.goto("https://www.globalsqa.com/demo-site/draganddrop/");
+    //await page.waitForTimeout(10000)
     const frame=page.frameLocator('[rel-title="Photo Manager"] iframe')
     const Distination= frame.locator('#trash')
     await frame.locator('li', {hasText:'High Tatras 2'}).dragTo(Distination)
@@ -17,7 +18,10 @@ test("open Drag & Drop Site URL", async ({ page }) => {
    await page.mouse.up()
     
 
+   //await frame.locator('#trash li h5').first().waitForElementState('visible');
+   //wait
+  // await frame.locator('#trash li h5').first().waitFor();
    // assertion by array with two dim. with to inserted value
-   expect(frame.locator('#trash li h5')).toHaveText(["High Tatras 2","High Tatras 4"])
+    await expect(frame.locator('#trash li h5')).toHaveText(["High Tatras 2","High Tatras 4"])
 
 })
